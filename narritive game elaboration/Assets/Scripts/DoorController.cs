3f9e08327bedc8a0b1e9e3578b365dd3df327b21
Robert Chen <chen1fan@icloud.com>
2019-03-07 10:13:10 -0500

    initial the project

---
+++ narritive game elaboration/Assets/Scripts/DoorController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float teleportX;
+
+    public float teleportY;
+
+    public bool faceupordown; // true = face up;
+
+    private bool playerfaceup;
+
+    private bool playerfacedown;
+
+    private GameObject player;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(PlayerMovement.FaceDirection == 0)
+        {
+            playerfacedown = true;
+        }
+        else if (PlayerMovement.FaceDirection == 1)
+        {
+            playerfaceup = true;
+        }
+        else
+        {
+            playerfacedown = false;
+            playerfaceup = false;
+        }
+
+
+
+
+
+
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision2D)
+    {
+       if(collision2D.gameObject.name == "Player")
+        {
+
+            if(faceupordown == true && playerfaceup == true)
+            {
+                Teleport();
+            }
+            else if(faceupordown == false && playerfacedown == true)
+            {
+                Teleport();
+            }
+
+
+
+
+
+            //Teleport();
+           // Debug.Log("touch door");
+
+        }
+    }
+
+    void Teleport()
+    {
+        player.transform.position = new Vector3(teleportX,teleportY,0);
+    }
+}
---
+++ narritive game elaboration/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float playerMovementSpeed;
+    public Animator animator;
+    public static int FaceDirection;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+
+            transform.position += new Vector3(playerMovementSpeed*Time.deltaTime, 0);
+
+            FaceDirection = 2;
+            walking();
+
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+
+            transform.position += new Vector3(-playerMovementSpeed * Time.deltaTime, 0);
+
+            FaceDirection = 3;
+            walking();
+        }
+        else if (Input.GetAxis("Vertical") > 0)
+        {
+
+            transform.position += new Vector3(0, playerMovementSpeed * Time.deltaTime);
+
+            FaceDirection = 1;
+            walking();
+
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+
+            transform.position += new Vector3(0, -playerMovementSpeed * Time.deltaTime);
+
+            FaceDirection = 0;
+            walking();
+        }
+        else {
+            animator.SetBool("ifWalk", false);
+        }
+        animator.SetInteger("FaceDirection", FaceDirection);
+
+        void walking()
+        {
+            animator.SetBool("ifWalk", true);
+        }
+
+
+    }
+}
